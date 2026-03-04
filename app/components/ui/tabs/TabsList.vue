eb31228f359fb1f405e4a9ec46d938453ee4c655
nirajpdn <niraj.pradhan.393@gmail.com>
2026-03-04 17:01:14 +0545

    fix: tailwindcss, shadcn components

---
+++ app/components/ui/tabs/TabsList.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import type { TabsListProps } from "reka-ui";
+import type { HTMLAttributes } from "vue";
+import { reactiveOmit } from "@vueuse/core";
+import { TabsList } from "reka-ui";
+import { cn } from "@/lib/utils";
+
+const props = defineProps<
+  TabsListProps & { class?: HTMLAttributes["class"] }
+>();
+
+const delegatedProps = reactiveOmit(props, "class");
+</script>
+
+<template>
+  <TabsList
+    data-slot="tabs-list"
+    v-bind="delegatedProps"
+    :class="
+      cn(
+        'inline-flex h-10 items-center justify-center rounded-md bg-muted p-1 text-muted-foreground',
+        props.class,
+      )
+    "
+  >
+    <slot />
+  </TabsList>
+</template>
